09f9796d32e0c8e227ee5dbaa534d37ef476e488
abeshi22 <abeshi22@gmail.com>
2022-02-10 23:22:42 +0900

    ver1.0

--- GuessNumber.js
+++ GuessNumber.js
@@ -2,14 +2,20 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 let answer = 0;
+let rader = new Array(100);
+for (let i=0; i<100; i++){
+    rader[i] = true;
+}
 
 function drawRader(){
     for(let i=0; i<100 ;i++){
-        ctx.beginPath();
-        ctx.rect(5*i+10,20,4,30);
-        ctx.fillStyle = "#00ff00";
-        ctx.fill();
-        ctx.closePath();
+        if(rader[i]==true){
+            ctx.beginPath();
+            ctx.rect(5*i+10,20,4,30);
+            ctx.fillStyle = "#00ff00";
+            ctx.fill();
+            ctx.closePath();
+        }
     }
     ctx.font = "16px Arial";
     ctx.fillStyle = "#000000";
@@ -17,6 +23,7 @@ function drawRader(){
     ctx.fillText("99",500,15);
 }
 
+// 答えの設定
 function setAnswer(){
     answer = Math.floor(Math.random()*100);
     console.log(answer);
@@ -27,6 +34,7 @@ function hint(){
     document.getElementById("hint").innerText = "答えを設定しました。\n";
 }
 
+// 数字ボタンとBSボタンを押したときの処理
 function push(val){
         let temp = document.getElementById("input_answer").value;
     if(val=="bs"){
@@ -42,6 +50,8 @@ function push(val){
     }
 }
 
+// 決定ボタンを押したときの処理
+// 予想が正しいかの判定
 function guessNumber(){
     if(document.getElementById("input_answer").value == ""){
         alert("答えを入力してください");
@@ -52,10 +62,16 @@ function guessNumber(){
         var guess = Number(document.getElementById("input_answer").value);
         if(guess < answer){
             document.getElementById("hint").innerText += ("答えは" + guess + "より大きいです。\n");
+            for(let i=0; i<=guess; i++){rader[i]=false;}
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            drawRader();
             document.getElementById("input_answer").value = "";
             document.getElementById("hint").scrollTo(0, document.getElementById("hint").scrollHeight);
         }else if(guess > answer){
             document.getElementById("hint").innerText += ("答えは" + guess + "より小さいです。\n");
+            for(let i=guess; i<100; i++){rader[i]=false;}
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            drawRader();
             document.getElementById("input_answer").value = "";
             document.getElementById("hint").scrollTo(0, document.getElementById("hint").scrollHeight);
         }else{
@@ -70,5 +86,4 @@ function guessNumber(){
 // メインの処理
 drawRader();
 setAnswer();
-// pushNumber();
 hint();
