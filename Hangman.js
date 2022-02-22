c20ae0ff0b61e75148d7e3297973b8179ce9b7c8
abeshi22 <abeshi22@gmail.com>
2022-02-23 02:12:21 +0900

    ver 1.0

--- Hangman.js
+++ Hangman.js
@@ -3,7 +3,7 @@ let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
 let answer = "";            //正解の単語
-let correctAnswer = false;  //解答状況フラグ
+let correct = [];  //解答状況フラグ
 let alphabet = "";          //未使用文字
 let mistakeCount = 0;       //誤答回数
 
@@ -19,37 +19,86 @@ let answerWord = ["BOY", "CAT", "FOX", "HAT", "TEN",
 // 答えの設定
 function setAnswer(){
     answer = answerWord[Math.floor(Math.random()*answerWord.length)];
-    console.log(answer);
+    for (let i=0; i<answer.length; i++){
+        correct[i] = false;
+    }
+    // console.log(answer);
 }
 
 
 function hint(){
-    document.getElementById("hint").innerText = "答えを設定しました。\n";
+    document.getElementById("hint1").innerText = "答えを設定しました。\n";
     for (let i=0; i<answer.length; i++){
-        document.getElementById("hint").innerText += " _";
+        document.getElementById("hint2").innerText += "_";
     }
-    console.log(document.getElementById("hint").innerText);
 }
-
-
-
 
 
 
 // アルファベットボタンとBSボタンを押したときの処理
 // 予想が正しいかの判定
 function checkAnswer(val){
+    let check = false;
     for (let i=0; i<answer.length; i++){
         if(val == answer.substr(i,1)){
-            console.log(val);
+            check = true;
+            correct[i] = true;
+            document.getElementById("submit"+val).style.backgroundColor = "#ff5555";
+            document.getElementById("submit"+val).style.color = "yellow";
         }
     }
+    if(check == false){
+        mistakeCount++;
+        document.getElementById("submit" + val).style.backgroundColor = "#ffffff";
+        document.getElementById("submit" + val).style.color = "#dddddd";
+        document.getElementById("submit" + val).style.borderColor = "#dddddd";
+        document.getElementById("submit" + val).disabled = true;
+    }
 
+    if(checkCorrect()) {document.getElementById("hint1").innerText = "正解です！"}
+    
+    showStatus();
+}
+
+// 文字をすべて当てられたかチェック
+function checkCorrect(){
+    for(let i=0; i<answer.length; i++){
+        if(correct[i]==false){
+            return false;
+        }
+    }
+    return true;
+}
+
+function showStatus(){
+    document.getElementById("hint2").innerText = "";
+    for (let i=0; i<answer.length; i++){
+        if(correct[i]==true){
+            document.getElementById("hint2").innerText += answer.substr(i,1);
+        }else{
+            document.getElementById("hint2").innerText += "_";
+        }
+    }
+    document.getElementById("life").innerText = "のこり" + (10-mistakeCount) + "トライ";
+    
+    if(mistakeCount==10){
+        document.getElementById("hint1").innerText = "残念！正解は" + answer + "でした！";
+        document.getElementById("hint2").innerText = answer;
+    }
+}
+
+function giveup(){
+    document.getElementById("hint1").innerText = "ギブアップ！正解は" + answer + "でした！";
+    document.getElementById("hint2").innerText = answer;
 }
 
 function reset(){
+    mistakeCount = 0;
+    document.getElementById("hint1").innerText = "";
+    document.getElementById("hint2").innerText = "";
     setAnswer();
     hint();
+    showStatus();
 }
 
 
